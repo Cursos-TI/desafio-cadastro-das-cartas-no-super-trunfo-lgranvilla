e12fea8a4c68bcd5c7ce738b9dbff159efb2b5bb
lgranvilla <l.granvilla@gmail.com>
2025-02-26 13:37:29 +0000

    *** Adicionado comentarios nas de área e PIB para o não uso de virgula nas casas decimais. *** Alteração do printF(população) com a troca da palavra "pessoas" para "de habitantes" *** Inserido printf para titulo do desafio. *** Incluido quebra de linha \n nas linhas 21, 45, 69 para melhorar a vizualização da saida de dados para o usuário.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
 int main() {
+
+    printf("\n*** DESAFIO SUPER TRUNFO. NIVEL: NOVATO***\n");
+
     // Declaração das variáveis para as duas cartas
+    
     char estado_1, estado_2;  
     char codigo_da_carta_1[20], codigo_da_carta_2[20];
     char nome_da_cidade_1[50], nome_da_cidade_2[50];
@@ -11,8 +15,10 @@ int main() {
     int pontos_turisticos_1, pontos_turisticos_2;
 
     // Entrada de dados para a primeira carta
-    printf("*** Digite os dados da Carta 1 ***\n");
-    printf("Digite a letra do Estado: \n");
+    
+    printf("\n*** Digite os dados da Carta 1 ***\n");
+    
+    printf("\nDigite a letra do Estado: \n");
     scanf(" %c", &estado_1);
 
     printf("Digite o código da carta: \n");
@@ -22,20 +28,21 @@ int main() {
     scanf("%s", nome_da_cidade_1);
 
     printf("Digite a população: \n");
-    scanf("%d", &populacao_1);
+    scanf("%d", &populacao_1);        
 
     printf("Digite a área: \n");
-    scanf("%f", &area_1);
+    scanf("%f", &area_1);             //Não utilizar virgua nas casas decimais//
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib_1);
+    scanf("%f", &pib_1);              //Não utilizar virgua nas casas decimais//
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos_1);
 
     // Entrada de dados para a segunda carta
+    
     printf("\n *** Digite os dados da Carta 2 ***\n");
-    printf("Digite a letra do Estado: \n");
+    printf("\nDigite a letra do Estado: \n");
     scanf(" %c", &estado_2);
 
     printf("Digite o código da carta: \n");
@@ -48,31 +55,33 @@ int main() {
     scanf("%d", &populacao_2);
 
     printf("Digite a área: \n");
-    scanf("%f", &area_2);
+    scanf("%f", &area_2);             //Não utilizar virgua nas casas decimais//
 
     printf("Digite o PIB: \n");
-    scanf("%f", &pib_2);
+    scanf("%f", &pib_2);              //Não utilizar virgua nas casas decimais//
 
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos_2);
 
-    // Exibição dos dados das duas cartas
+    // Saída de dados da carta 1//
+    
     printf("\n=== Dados da Carta 1 ===\n");
-    printf("Código: %s\n", codigo_da_carta_1);
+    printf("\nCódigo: %s\n", codigo_da_carta_1);
     printf("Estado: %c\n", estado_1);
     printf("Cidade: %s\n", nome_da_cidade_1);
-    printf("População: %d pessoas\n", populacao_1);
+    printf("População: %d de habitantes.\n", populacao_1);
     printf("Área: %.2f km²\n", area_1);
-    printf("PIB: R$ %.2f bilhões\n", pib_1);
+    printf("PIB: R$ %.2f bilhões de reais\n", pib_1);
     printf("Pontos turísticos: %d\n", pontos_turisticos_1);
 
+    // Saida de dados da carta dois//
     printf("\n=== Dados da Carta 2 ===\n");
-    printf("Código: %s\n", codigo_da_carta_2);
+    printf("\nCódigo: %s\n", codigo_da_carta_2);
     printf("Estado: %c\n", estado_2);
     printf("Cidade: %s\n", nome_da_cidade_2);
-    printf("População: %d pessoas\n", populacao_2);
+    printf("População: %d de habitantes.\n", populacao_2);
     printf("Área: %.2f km²\n", area_2);
-    printf("PIB: R$ %.2f bilhões\n", pib_2);
+    printf("PIB: R$ %.2f bilhões de reais\n", pib_2);
     printf("Pontos turísticos: %d\n", pontos_turisticos_2);
 
     return 0;
