f79beea510dc0cecdc76b25cdb1823d8f3af3ffb
lgranvilla <l.granvilla@gmail.com>
2025-03-26 00:35:28 +0000

    ***Efetuado a atualização do Nível no título do trabalho
    *** Inserido as variáveis 'densidade' e 'perCapita' para cada carta para aplicação de novos recursos.
    
    *** Variáveis inicializadas com os dados inseridos pelo usuário e atribuído operador automático para cálculo da densidade populacional e PIB perCapita de cada carta.
    
    *** Inserido as linhas de Printf para impressão do resultado dos cálculos de densidade populacional e PIB per Capita para cada carta de maneira simplificada para o usuário.

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -2,7 +2,7 @@
 
 int main() {
 
-    printf("\n*** DESAFIO SUPER TRUNFO. NIVEL: NOVATO***\n");
+    printf("\n*** DESAFIO SUPER TRUNFO. NIVEL: AVENTUREIRO***\n"); // Atuallização de Nivel do trabalho!
 
     // Declaração das variáveis para as duas cartas
     
@@ -13,6 +13,9 @@ int main() {
     float area_1, area_2;
     float pib_1, pib_2;
     int pontos_turisticos_1, pontos_turisticos_2;
+    float densidade1, perCapita1; // Inserção das variáveis para o calculo de densidade popilacional e renda per capta para carta 1
+    float densidade2, perCapita2; // Inserção das variáveis para o calculo de densidade popilacional e renda per capta para carta 2
+
 
     // Entrada de dados para a primeira carta
     
@@ -39,6 +42,9 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos_1);
 
+    densidade1 = populacao_1 / area_1; // Váriavel inicializada com os dados inseridos pelo usuário e aplicado o operador aritimético para calculo de Densidade populacional da carta 1
+    perCapita1 = pib_1 / populacao_1;  // Váriavel inicializada com os dados inseridos pelo usuário e aplicado o operador aritimético para calculo da Renda Per Capta da carta 1
+
     // Entrada de dados para a segunda carta
     
     printf("\n *** Digite os dados da Carta 2 ***\n");
@@ -63,6 +69,9 @@ int main() {
     printf("Digite o número de pontos turísticos: \n");
     scanf("%d", &pontos_turisticos_2);
 
+    densidade2 = populacao_2 / area_2;  // Váriavel inicializada com os dados inseridos pelo usuário e aplicado o operador aritimético para calculo de Densidade populacional da carta 2
+    perCapita2 = pib_2 / populacao_2;  // Váriavel inicializada com os dados inseridos pelo usuário e aplicado o operador aritimético para calculo da Renda Per Capta da carta 2
+
     // Saída de dados da carta 1//
     
     printf("\n=== Dados da Carta 1 ===\n");
@@ -73,6 +82,8 @@ int main() {
     printf("Área: %.2f km²\n", area_1);
     printf("PIB: R$ %.2f bilhões de reais\n", pib_1);
     printf("Pontos turísticos: %d\n", pontos_turisticos_1);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade1); //Impressão da nova funcionalidade: Calculo da densidade populacional da carta 1
+    printf("Pib per Capita: %.2f Reais\n", perCapita1);  //Impressão da nova funcionalidade: Calculo da renda per capta da carta 1
 
     // Saida de dados da carta dois//
     printf("\n=== Dados da Carta 2 ===\n");
@@ -83,6 +94,9 @@ int main() {
     printf("Área: %.2f km²\n", area_2);
     printf("PIB: R$ %.2f bilhões de reais\n", pib_2);
     printf("Pontos turísticos: %d\n", pontos_turisticos_2);
+    printf("Densidade Populacional: %.2f hab/km²\n", densidade2);  //Impressão da nova funcionalidade: Calculo da densidade populacional da carta 2
+    printf("Pib per Capita: %.2f Reais\n", perCapita2);  //Impressão da nova funcionalidade: Calculo da renda per capta da carta 2
+
 
     return 0;
 }
